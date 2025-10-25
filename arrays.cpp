3a98196c07323dda8dbe77098ba39e8fbf42a908
marianofacultad23 <marianofacultad23@gmail.com>
2025-10-25 11:48:16 -0300

    Add files via upload

--- arrays.cpp
+++ arrays.cpp
@@ -3,8 +3,28 @@ using namespace std;
 
 int main (){
 
-    string automovil = " Chevrolet - Spinn";
-    cout << automovil<< endl;
+  /*
+  string automovil[] = {"Chevrolet","Audi", "VW"};
+    
+cout << automovil[0] << endl;
+cout << automovil[1] << endl;
+cout << automovil[2] << endl;
+
+automovil [0] = "Volvo";
+cout << automovil [0];
+  */  
+
+  string autos [3];
+  autos [0] = "Chevrolet";
+  autos [1] = "Audi";
+  autos [2] = "VW";
+
+  double precios [] = {5.00,8.50,9.99,15.00};
+cout << precios [0] << endl;
+cout << precios [1] << endl;
+cout << precios [2] << endl;
+cout << precios [3] << endl;
+
 
     return 0;
 }
